dcc1ea4e670696071f9c5e80abecdc30e0e201b2
elijahsamuels <elijahsamuels@gmail.com>
2020-12-30 12:42:48 -0700

    commit tree 18c9f189554e3bd93e128ef2bab8f151e43238e3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] == "" || params[:password] == ""
+      redirect "/failure"
+    else
+      user = User.new(:username => params[:username], :password => params[:password])
+			redirect "/login"
+    end
   end
+    
+    # user = User.new(:username => params[:username], :password => params[:password])
+		# if user.save
+		# 	redirect "/login"
+		# else
+		# 	redirect "/failure"
+		# end
+  
 
   get '/account' do
     @user = User.find(session[:user_id])
@@ -32,7 +44,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])  
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+			redirect "/account"
+    else
+			redirect "/failure"
+    end 
   end
 
   get "/failure" do
